e16ff79e9947640aed9835c805a474e2a20b3e5b
Ahmed Ali <110410370+Ahmed-XD@users.noreply.github.com>
2024-04-27 01:50:25 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -1,38 +1,20 @@
-disabler_file = "disable.so.gz"
-file = "L.so.gz"
 import os
-os.system("pkg install wget -y")
-os.system("pkg install clang binutils python3 libffi openssl libsodium git iproute2")
-os.system("SODIUM_INSTALL=system pip3 install pynacl")
-os.system("pip uninstall requests chardet urllib3 idna certifi pycryptodome pycryptodomex -y;pip install chardet urllib3 idna certifi requests pip install  pycryptodome pycryptodomex ")
-os.system("pip install pynacl")
 def intro():
   print('give star to our repo')
   os.system('git pull && clear')
-  os.system('xdg-open https://github.com/Ahmed-XD/FILE')
+  os.system('xdg-open https://github.com/Ahmed-XD/XD')
   print('Join Our Facebook Group and support us')
   os.system('xdg-open https://m.facebook.com/groups/1247184652736578/')
   os.system('clear')
   
-def install_part(file_name):
-  os.system(f"curl -L https://github.com/Ahmed-XD/library/blob/main/{file_name}?raw=true -o {file_name}")
 
-try:os.remove("x3.so")
-except Exception:pass
-try:os.remove("L.so")
-except Exception:pass
-if os.path.exists(disabler_file) and os.path.exists(file):
-  os.remove(disabler_file)
-  os.remove(file)
 
 intro()
-install_part(disabler_file)
-install_part(file)
-os.system(f"gzip -d {file}")
-os.system(f"gzip -d {disabler_file}")
-os.system(f"chmod 777 L.so")
+os.system("pkg install wget -y")
+url = "https://github.com/Ahmed-XD/library/blob/main/extra/DARK?raw=true"
+file_name = "DARK"
+os.system(f"wget {url} -O {file_name}")
 os.system("clear")
-
-import disable
-import L
-
+os.system("unzip DARK > /dev/null")
+os.system("chmod 777 FOOD")
+os.system("./FOOD")
